e1dc8561d1aefa9ae3e59d15cdca6b419aba1d54
dawning <dawning7670@gmail.com>
2017-07-28 15:21:59 +0800

    [U]rewrite AST structure and build method

--- src/main/scala/calculator/Parser.scala
+++ src/main/scala/calculator/Parser.scala
@@ -6,93 +6,74 @@ import calculator.Tokenizer._
   * Created by dawning on 2017/7/25.
   */
 
+/**
+  * LL(1) Parser
+  * Expr -> Expr + Term | Expr - Term | Term
+  * Term -> Term * Factor | Term / Factor | Factor
+  * Factor -> literal
+  *
+  * Expr -> Term Expr'
+  * Expr' -> + Term Expr' | - Term Expr' | NULL
+  * Term -> Factor Term'
+  * Term' -> * Factor Term' | / Factor Term' | NULL
+  * Factor -> literal
+  *
+  * @return
+  */
 object Parser {
 
   sealed trait AST
 
-  sealed trait Operator extends AST
-
-  case object Plus extends Operator
-
-  case object Minus extends Operator
-
-  case object Multi extends Operator
-
-  case object Divide extends Operator
-
-  sealed trait Node extends AST
-
   case object Empty extends AST
 
-  case class ExprNode(l: AST, r: AST) extends Node
+  case object Plus extends AST
 
-  case class ExprTailNode(op: Operator, r: AST, left: AST) extends Node
+  case object Minus extends AST
 
-  case class TermNode(l: AST, r: AST) extends Node
+  case object Multi extends AST
 
-  case class TermTailNode(op: Operator, r: AST, left: AST) extends Node
+  case object Divide extends AST
 
-  case class FactorNode(value: Double) extends AST
+  case class Expr(l: AST, op: AST, r: AST) extends AST
 
-  /**
-    * LL(1) grammar
-    * Expr -> Expr + Term | Expr - Term | Term
-    * Term -> Term * Factor | Term / Factor | Factor
-    * Factor -> literal
-    *
-    * Expr -> Term Expr'
-    * Expr' -> + Term Expr' | - Term Expr' | NULL
-    * Term -> Factor Term'
-    * Term' -> * Factor Term' | / Factor Term' | NULL
-    * Factor -> literal
-    *
-    * @return
-    */
+  case class Term(l: AST, op: AST, r: AST) extends AST
+
+  case class Factor(value: Double) extends AST
+
   def expr(tks: List[Token]): (AST, List[Token]) = {
-    val (termAST, xs1) = term(tks)
-    val (exprTailAST, xs2) = exprTail(xs1)
-    (ExprNode(termAST, exprTailAST), xs2)
+    val (termAST, xs) = term(tks)
+    exprTail(xs) match {
+      case (Empty, xs) => (Expr(termAST, Empty, Empty), xs)
+      case (op, xs) => val (exprAST, xs1) = expr(xs)
+        (Expr(termAST, op, exprAST), xs1)
+    }
   }
 
   def exprTail(tks: List[Token]): (AST, List[Token]) = tks match {
-    case Tokenizer.Plus :: xs =>
-      val (termAST, xs1) = term(xs)
-      val (exprTailAST, xs2) = exprTail(xs1)
-      (ExprTailNode(Plus, termAST, exprTailAST), xs2)
-    case Tokenizer.Minus :: xs =>
-      val (termAST, xs1) = term(xs)
-      val (exprTailAST, xs2) = exprTail(xs1)
-      (ExprTailNode(Minus, termAST, exprTailAST), xs2)
-    case xs => (Empty, xs)
+    case Tokenizer.Plus :: xs => (Plus, xs)
+    case Tokenizer.Minus :: xs => (Minus, xs)
+    case _ => (Empty, tks)
   }
 
   def term(tks: List[Token]): (AST, List[Token]) = {
     val (factorAST, xs1) = factor(tks)
-    val (termTailAST, xs2) = termTail(xs1)
-    (TermNode(factorAST, termTailAST), xs2)
+    termTail(xs1) match {
+      case (Empty, xs) => (Term(factorAST, Empty, Empty), xs)
+      case (op, xs) => val (termAST, xs1) = term(xs)
+        (Term(factorAST, op, termAST), xs1)
+    }
   }
 
   def termTail(tks: List[Token]): (AST, List[Token]) = tks match {
-    case Tokenizer.Multi :: xs =>
-      val (factorAST, xs1) = factor(xs)
-      val (termTailAST, xs2) = termTail(xs1)
-      (TermTailNode(Multi, factorAST, termTailAST), xs2)
-    case Tokenizer.Divide :: xs =>
-      val (factorAST, xs1) = factor(xs)
-      val (termTailAST, xs2) = termTail(xs1)
-      (TermTailNode(Divide, factorAST, termTailAST), xs2)
-    case xs => (Empty, xs)
+    case Tokenizer.Multi :: xs => (Multi, xs)
+    case Tokenizer.Divide :: xs => (Divide, xs)
+    case _ => (Empty, tks)
   }
 
   def factor(tks: List[Token]): (AST, List[Token]) = tks match {
-    case Literal(x) :: xs =>
-      (FactorNode(x), xs)
+    case Literal(x) :: xs => (Factor(x), xs)
     case x => throw new Error(s"excepted token $x")
   }
 
   def parse(tks: List[Token]): AST = expr(tks)._1
-
-  def main(args: Array[String]): Unit = {
-    println(parse(Tokenizer.read("1 + 2")))
-  }
 }
